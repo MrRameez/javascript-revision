7b8287aa8c94183a74f7cb4385598f833473ad2c
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2025-06-14 01:58:43 +0500

    variable

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// node app.js
+
 const Name = "Rameez";
 
-console.log(`Hello, ${Name}!`);
+console.log(`Hello ${Name}!`);
+
+let numberValue = 3;
+
+var stringValue = "3";
+if (numberValue == stringValue) {
+  console.log("The values are equal with == operator");
+}
+
+if (numberValue === stringValue) {
+  console.log("The values are equal with === operator");
+}
+else {
+  console.log("The values are NOT equal with === operator");
+}
