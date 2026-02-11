87ddadcca9b75415b621ef83abc435fbf0c8c170
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2026-02-11 19:26:17 +0500

    Javascrpts Functions

---
+++ scripts.js
@@ -0,0 +1,107 @@
+// function abc(v1 = 3 , v2 = 4){ 
+    
+//     console.log(v1 + v2);
+// }
+
+// abc(5, 6); // 11
+// abc(5); // 9
+// abc(); // 7
+
+
+
+// const sum = function(a=3, b=4){
+
+//     console.log(a + b);
+// }
+
+// sum(5, 6); // 11
+// sum(5); // 9
+// sum(); // 7
+
+
+// const sum2 = (a=3, b=4) => {
+
+//     console.log(a + b);
+// }       
+
+// sum2(5, 6); // 11
+// sum2(5);    // 9
+// sum2();     // 7
+
+
+
+// function abc(val){
+
+//     console.log("this is nested function");
+//     val()
+    
+// }
+// abc(function(){
+//     console.log("First class function ");
+    
+// })
+
+
+
+
+
+// function abc(cal){
+//     return function(val){
+//         console.log("this is nested function",val);
+//        cal()
+//     }
+// }
+
+// abc(function(){
+//     console.log("high order function");
+// })(32)
+
+
+
+
+
+// function abc(a,b,c,...v){
+//     console.log("Thsi is rest perameter ", a,b,c,v);
+    
+// }
+
+
+// abc(1,2,3,4,5,6,7,8,9,10)
+
+
+// abc()
+
+// function abc(){
+//     console.log("function statement ");
+    
+// }
+
+
+// abc();
+
+
+// let abc = function(){
+//     console.log("function expression");
+// }
+
+
+
+// const multiply = (a,b) => {
+//     return a * b
+// };
+
+
+// console.log(multiply(5,6));
+
+
+
+
+// let i = 0
+
+// while (i++<3) {
+//     console.log(i)
+// }
+
+
+
+
